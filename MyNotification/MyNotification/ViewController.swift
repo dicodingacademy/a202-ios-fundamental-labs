0707bdef38359f9123dd736f252a5c4bab5d2494
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-03-25 13:46:47 +0700

    Update MyNotification Source Code

--- MyNotification/MyNotification/ViewController.swift
+++ MyNotification/MyNotification/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 import UserNotifications
 
 class ViewController: UIViewController {
-        
+
     @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         UNUserNotificationCenter.current().delegate = self
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
             if granted {
                 print("Mendapatkan izin dari pengguna untuk local notifications")
             } else {
@@ -24,24 +24,27 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+
     @IBAction func scheduleNotification(_ sender: Any) {
-        
+
         let content = UNMutableNotificationContent()
         content.title = "Submission Anda Telah Diterima!"
         content.body = "Selamat Anda telah menyelesaikan kelas Belajar Fundamental Aplikasi iOS."
         content.sound = .default
         content.userInfo = ["value": "Data dengan local notification"]
-        
-        let fireDate = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second], from: Date().addingTimeInterval(10))
-        
+
+        let fireDate = Calendar.current.dateComponents(
+            [.day, .month, .year, .hour, .minute, .second],
+            from: Date().addingTimeInterval(10)
+        )
+
         let trigger = UNCalendarNotificationTrigger(dateMatching: fireDate, repeats: false)
         // let anotherTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 20, repeats: false)
-        
+
         let request = UNNotificationRequest(identifier: "message", content: content, trigger: trigger)
-        
+
         let center = UNUserNotificationCenter.current()
-        center.add(request) { (error) in
+        center.add(request) { error in
             if error != nil {
                 print("Error = \(error?.localizedDescription ?? "Terjadi kesalahan dalam local notification")")
             }
@@ -50,14 +53,20 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
         completionHandler([.alert, .badge, .sound])
     }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
         print("UserInfo yang terkait dengan notifikasi == \(response.notification.request.content.userInfo)")
-        
         completionHandler()
     }
 }
