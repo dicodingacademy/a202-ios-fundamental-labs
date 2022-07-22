f5f23487d4a048ba71802689a5f4b0f3758c8bfe
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-07-22 16:33:43 +0700

    Update Code

--- LatihanTrackLocation/MyTrackLocation/ViewController.swift
+++ LatihanTrackLocation/MyTrackLocation/ViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
-import CoreLocation
 import MapKit
 
 class ViewController: UIViewController {
 
+  // MARK: Inisialisasi MapView.
   @IBOutlet weak var mapView: MKMapView!
 
+  // MARK: Inisialisasi Array MKPointAnnotion untuk menampung pin yang nantinya dimunculkan di MapView.
   private var locations: [MKPointAnnotation] = []
 
+  // MARK: Mengatur Location Manager.
   private lazy var locationManager: CLLocationManager = {
     let manager = CLLocationManager()
     manager.delegate = self
@@ -25,6 +27,7 @@ class ViewController: UIViewController {
     return manager
   }()
 
+  // MARK: Menghidupkan dan mematikan Track Location.
   @IBAction func trackSwitch(_ sender: UISwitch) {
     if sender.isOn {
       locationManager.startUpdatingLocation()
@@ -34,15 +37,24 @@ class ViewController: UIViewController {
   }
 }
 
+// MARK: Ini adalah implementasi dari CLLocationManagerDelegate.
 extension ViewController: CLLocationManagerDelegate {
 
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+  func locationManager(
+    _ manager: CLLocationManager,
+    didUpdateLocations locations: [CLLocation]
+  ) {
+    // MARK: Mendapatkan lokasi terakhir pengguna.
     guard let mostRecentLocation = locations.last else { return }
 
+    // MARK: Membuat variabel untuk menampung latitude dan longitude.
     let annotationToAdd = MKPointAnnotation()
     annotationToAdd.coordinate = mostRecentLocation.coordinate
 
+    // MARK: Menambahkan lokasi terakhir ke array Locations.
     self.locations.append(annotationToAdd)
+
+    // MARK: Ketika titik lebih dari 50 akan dihapus dari MapView
     while self.locations.count > 50 {
       if let annotationToRemove = self.locations.first {
         self.locations.remove(at: 0)
@@ -50,9 +62,11 @@ extension ViewController: CLLocationManagerDelegate {
       }
     }
 
-    if UIApplication.shared.applicationState == .active {
-      mapView.showAnnotations(self.locations, animated: true)
-    } else {
+    // MARK: Memunculkan lokasi terakhir di MapView.
+    mapView.showAnnotations(self.locations, animated: true)
+
+    // MARK: Ketika Track Location berjalan di background akan mencetak informasi detailnya.
+    if UIApplication.shared.applicationState == .background {
       print("Aplikasi dalam background state. Lokasi baru saat ini ", mostRecentLocation)
     }
   }
