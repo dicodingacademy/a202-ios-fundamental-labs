334cfd41efb8bbc7afb770b6318157cc4575df78
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-08-10 16:23:59 +0700

    Add Project

---
+++ LatihanMenguduhGambarDenganAsyncAwait/LatihanMenguduhGambar/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  LatihanMenguduhGambar
+//
+//  Created by Gilang Ramadhan on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet var movieTableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    movieTableView.dataSource = self
+
+    movieTableView.register(
+      UINib(nibName: "MovieTableViewCell", bundle: nil),
+      forCellReuseIdentifier: "movieTableViewCell"
+    )
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    return movies.count
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    if let cell = tableView.dequeueReusableCell(
+      withIdentifier: "movieTableViewCell",
+      for: indexPath
+    ) as? MovieTableViewCell {
+      
+      let movie = movies[indexPath.row]
+      cell.movieTitle.text = movie.title
+      cell.movieImage.image = movie.image
+
+      if movie.state == .new {
+        cell.indicatorLoading.isHidden = false
+        cell.indicatorLoading.startAnimating()
+        startDownload(movie: movie, indexPath: indexPath)
+      } else {
+        cell.indicatorLoading.stopAnimating()
+        cell.indicatorLoading.isHidden = true
+      }
+
+      return cell
+    } else {
+      return UITableViewCell()
+    }
+  }
+
+  fileprivate func startDownload(movie: Movie, indexPath: IndexPath) {
+    let imageDownloader = ImageDownloader()
+    if movie.state == .new {
+      do {
+        let image = try await imageDownloader.downloadImage(url: movie.poster)
+        movie.state = .downloaded
+        movie.image = image
+        self.movieTableView.reloadRows(at: [indexPath], with: .automatic)
+      } catch {
+        movie.state = .failed
+        movie.image = nil
+      }
+    }
+  }
+
+}
