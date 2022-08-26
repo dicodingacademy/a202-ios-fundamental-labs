464c9757090901be7dd8c6a47e4a9788482c3882
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-08-26 18:11:06 +0700

    Update Code

--- LatihanGETMethod.playground/Contents.swift
+++ LatihanGETMethod.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 // MARK: Gunakan API Key dalam akun Anda.
-let apiKey = ""
+let apiKey = "b8e51a152ed387c46ba2d4c8ced7345d"
 let language = "en-US"
 let page = "1"
 
@@ -20,11 +20,85 @@ let task = URLSession.shared.dataTask(with: request) { data, response, error in
 
   if let data = data {
     if response.statusCode == 200 {
-      print("DATA: \(data)")
+      decodeJSON(from: data)
     } else {
-      print("ERROR: \(data), Http Status: \(response.statusCode)")
+      print("ERROR: \(data), HTTP Status: \(response.statusCode)")
     }
   }
 }
 
 task.resume()
+
+func decodeJSON(from data: Data) {
+  let decoder = JSONDecoder()
+
+  if let movies = try? decoder.decode(MovieResponses.self, from: data) as MovieResponses {
+    print("PAGE: \(movies.page)")
+    print("TOTAL RESULTS: \(movies.totalResults)")
+    print("TOTAL PAGES: \(movies.totalPages)")
+
+    movies.movies.forEach { movie in
+      print("TITLE: \(movie.title)")
+      print("POSTER: \(movie.posterPath)")
+      print("DATE: \(movie.releaseDate)")
+    }
+
+  } else {
+    print("ERROR: Can't Decode JSON")
+  }
+}
+
+struct MovieResponses: Codable {
+  let page: Int
+  let totalResults: Int
+  let totalPages: Int
+  let movies: [MovieResponse]
+
+  enum CodingKeys: String, CodingKey {
+    case page
+    case totalResults = "total_results"
+    case totalPages = "total_pages"
+    case movies = "results"
+  }
+}
+
+struct MovieResponse: Codable {
+  let popularity: Double
+  let posterPath: String
+  let title: String
+  let genres: [Int]
+  let voteAverage: Double
+  let overview: String
+  let releaseDate: Date
+
+  enum CodingKeys: String, CodingKey {
+    case popularity
+    case posterPath = "poster_path"
+    case title
+    case genres = "genre_ids"
+    case voteAverage = "vote_average"
+    case overview
+    case releaseDate = "release_date"
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    // Menentukan alamat gambar
+    let path = try container.decode(String.self, forKey: .posterPath)
+    posterPath = "https://image.tmdb.org/t/p/w300\(path)"
+
+    // Mementukan tanggal rilis
+    let dateString = try container.decode(String.self, forKey: .releaseDate)
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    releaseDate = dateFormatter.date(from: dateString)!
+
+    // Untuk properti lainnya, cukup disesuaikan saja.
+    popularity = try container.decode(Double.self, forKey: .popularity)
+    title = try container.decode(String.self, forKey: .title)
+    genres = try container.decode([Int].self, forKey: .genres)
+    voteAverage = try container.decode(Double.self, forKey: .voteAverage)
+    overview = try container.decode(String.self, forKey: .overview)
+  }
+}
