2c81e8cb2bfb8c1b95c8b79e57cd79c7f844d472
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-08-30 14:28:28 +0700

    Move Location Project

--- LatihanMembagikanFile/DicodingDrop/ViewController.swift
+++ LatihanMembagikanFile/DicodingDrop/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 }
