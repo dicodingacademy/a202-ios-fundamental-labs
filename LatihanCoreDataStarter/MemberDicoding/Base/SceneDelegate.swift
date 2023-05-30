657b01c95d12c4ef5c873ce3f2a1202379e9c135
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2023-05-30 10:18:50 +0700

    Update SceneDelegate.swift

--- LatihanCoreDataStarter/MemberDicoding/Base/SceneDelegate.swift
+++ LatihanCoreDataStarter/MemberDicoding/Base/SceneDelegate.swift
@@ -11,5 +11,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
+  
+  func scene(
+    _ scene: UIScene, 
+    willConnectTo session: UISceneSession, 
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    // Insert data dummy
+  }
 
 }
